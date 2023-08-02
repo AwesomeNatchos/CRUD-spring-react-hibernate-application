e99d788228443e9b81c272efaeb21000f51bc371
@awesomenatchos <natcha.uthaug@gmail.com>
2023-08-03 01:49:42 +0200

    added axios

---
+++ front/src/pages/Home.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        console.log("page is loading");
+        loadUsers();
+    },[]);
+
+
+    const loadUsers =  async() =>{
+        const result = await axios.get("http://localhost:8080/users");
+        console.log(result);
+        setUsers(result.data);
+    }
+
+
+
+
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+            <thead>
+                <tr>
+                <th scope="col">ID</th>
+                <th scope="col"> Name </th>
+                <th scope="col"> Username </th>
+                <th scope="col"> Email </th>
+                <th scope="col"> Address </th>
+                <th scope="col"> Action </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                {
+                    users.map((user, index)=>(
+                        <tr>
+                            <th scope="row" key={index}>  { index+1 }</th>
+                            <td> { user.id } </td>
+                            <td> { user.name  } </td>
+                            <td> { user.username} </td>
+                            <td> { user.email} </td>
+                            <td> { user.address } </td>
+                        </tr>
+                   
+                    ))
+                }
+
+               
+            </tbody>
+            </table>
+
+        </div>
+
+        
+
+    </div>
+  )
+}
